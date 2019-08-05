ac7009e188b14d37bd348e8396e7945241e3203d
Phong-Luong <vien.luong@axa.fr>
2019-08-05 14:49:02 +0200

    bump package

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-// import { Form } from 'studio-form'
+import { Form } from 'studio-form'
 
 import InputText from './InputText'
 import InputDate from './InputDate'
@@ -20,115 +20,114 @@ const StyledText = styled(Text.h2)({
 })
 
 const MyForm = ({ inline = false, ...rest }) => {
-  return 'test removing studio-form'
-  // return (
-  //   <Form
-  //     submitButton={() => {
-  //       return <Button type="submit">Envoyer</Button>
-  //     }}
-  //     components={props => {
-  //       const { uiProperty, property, name } = props
+  return (
+    <Form
+      submitButton={() => {
+        return <Button type="submit">Envoyer</Button>
+      }}
+      components={props => {
+        const { uiProperty, property, name } = props
 
-  //       let childrenProps = {}
-  //       let currentProps = props
+        let childrenProps = {}
+        let currentProps = props
 
-  //       let Child = undefined
-  //       let beforeChild = undefined
+        let Child = undefined
+        let beforeChild = undefined
 
-  //       if (property.default) {
-  //         childrenProps.value = property.default
-  //       }
+        if (property.default) {
+          childrenProps.value = property.default
+        }
 
-  //       // assign default props to each component input
-  //       if (uiProperty) {
-  //         if (uiProperty.placeholder) {
-  //           childrenProps.placeholder = uiProperty.placeholder
-  //         }
+        // assign default props to each component input
+        if (uiProperty) {
+          if (uiProperty.placeholder) {
+            childrenProps.placeholder = uiProperty.placeholder
+          }
 
-  //         if (uiProperty.before) {
-  //           beforeChild = uiProperty.before
-  //         }
+          if (uiProperty.before) {
+            beforeChild = uiProperty.before
+          }
 
-  //         if (uiProperty.Component) {
-  //           Child = uiProperty.Component
-  //         }
-  //       }
+          if (uiProperty.Component) {
+            Child = uiProperty.Component
+          }
+        }
 
-  //       // property: date
-  //       if (property.type === 'date') {
-  //         Child =
-  //           uiProperty && uiProperty.widget === 'birthdate'
-  //             ? InputBirthdate
-  //             : InputDate
-  //         // format the default value before sending to the component
-  //         if (property.default) {
-  //           childrenProps.value = new Date(property.default)
-  //         }
-  //       }
+        // property: date
+        if (property.type === 'date') {
+          Child =
+            uiProperty && uiProperty.widget === 'birthdate'
+              ? InputBirthdate
+              : InputDate
+          // format the default value before sending to the component
+          if (property.default) {
+            childrenProps.value = new Date(property.default)
+          }
+        }
 
-  //       // property: enum
-  //       if (property.enum) {
-  //         if (uiProperty && uiProperty.enum) {
-  //           childrenProps.options = uiProperty.enum
-  //         } else {
-  //           childrenProps.options = filterOptions(property.enum)
-  //         }
+        // property: enum
+        if (property.enum) {
+          if (uiProperty && uiProperty.enum) {
+            childrenProps.options = uiProperty.enum
+          } else {
+            childrenProps.options = filterOptions(property.enum)
+          }
 
-  //         if (uiProperty && uiProperty.widget === 'select') {
-  //           Child = InputSelect
-  //           // override onChange because studio-form take { name, value } as a parameters
-  //           childrenProps.onChange = e => {
-  //             if (uiProperty.enum) {
-  //               props.onChange({ name, value: e, persist() {} })
-  //             } else {
-  //               props.onChange({ name, value: e.value, persist() {} })
-  //             }
-  //           }
+          if (uiProperty && uiProperty.widget === 'select') {
+            Child = InputSelect
+            // override onChange because studio-form take { name, value } as a parameters
+            childrenProps.onChange = e => {
+              if (uiProperty.enum) {
+                props.onChange({ name, value: e, persist() {} })
+              } else {
+                props.onChange({ name, value: e.value, persist() {} })
+              }
+            }
 
-  //           childrenProps.onBlur = e => {
-  //             if (uiProperty.enum) {
-  //               props.onBlur({ name, value: e, persist() {} })
-  //             } else {
-  //               props.onBlur({ name, value: e.value, persist() {} })
-  //             }
-  //           }
-  //           if (property.default) {
-  //             childrenProps.defaultValue = childrenProps.options.find(
-  //               enu => enu.value === property.default
-  //             )
-  //           }
-  //         } else {
-  //           childrenProps.big = true
-  //           Child = Radios
-  //         }
-  //       }
+            childrenProps.onBlur = e => {
+              if (uiProperty.enum) {
+                props.onBlur({ name, value: e, persist() {} })
+              } else {
+                props.onBlur({ name, value: e.value, persist() {} })
+              }
+            }
+            if (property.default) {
+              childrenProps.defaultValue = childrenProps.options.find(
+                enu => enu.value === property.default
+              )
+            }
+          } else {
+            childrenProps.big = true
+            Child = Radios
+          }
+        }
 
-  //       if (property.type === 'password') {
-  //         childrenProps.type = 'password'
-  //       }
-  //       // if no child is assigned, use a <InputText />
-  //       if (Child === undefined) {
-  //         Child = InputText
-  //         if (uiProperty && uiProperty.widget === 'textarea') {
-  //           childrenProps.vertical_align = 'top'
-  //           childrenProps.textarea = true
-  //         }
-  //       }
-  //       // assign children props
-  //       currentProps.childrenProps = childrenProps
+        if (property.type === 'password') {
+          childrenProps.type = 'password'
+        }
+        // if no child is assigned, use a <InputText />
+        if (Child === undefined) {
+          Child = InputText
+          if (uiProperty && uiProperty.widget === 'textarea') {
+            childrenProps.vertical_align = 'top'
+            childrenProps.textarea = true
+          }
+        }
+        // assign children props
+        currentProps.childrenProps = childrenProps
 
-  //       return (
-  //         <React.Fragment key={name}>
-  //           {beforeChild ? <StyledText>{beforeChild}</StyledText> : null}
-  //           <Field {...currentProps} inline={inline}>
-  //             <Child />
-  //           </Field>
-  //         </React.Fragment>
-  //       )
-  //     }}
-  //     {...rest}
-  //   />
-  // )
+        return (
+          <React.Fragment key={name}>
+            {beforeChild ? <StyledText>{beforeChild}</StyledText> : null}
+            <Field {...currentProps} inline={inline}>
+              <Child />
+            </Field>
+          </React.Fragment>
+        )
+      }}
+      {...rest}
+    />
+  )
 }
 
 export default MyForm
